0185de7e55041a85a7304433098dc80e7603bab0
曾立杰 <kobe830711@gmail.com>
2021-09-23 11:11:47 +0800

    update

--- application/views/Header.php
+++ application/views/Header.php
@@ -13,12 +13,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <link rel="stylesheet" href="https://images.plusone88.com/css/swiper-bundle.min.css">
     <meta name="format-detection" content="telephone=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="shortcut icon" href="#">
     <link href="https://images.plusone88.com/css/owl.carousel.min.css" rel="stylesheet" />
     <link href="https://images.plusone88.com/css/owl.theme.default.min.css" rel="stylesheet" />
     <script src="https://images.plusone88.com/js/swiper-bundle.min.js"></script>
     <script src="https://images.plusone88.com/js/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B7K6FMCW5L"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-B7K6FMCW5L');
+    </script>
  
 </head>
 
